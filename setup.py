f03de1b45069b8e480745666d5cbc55b99286ca8
zeeviiosub <57300859+zeeviiosub@users.noreply.github.com>
2019-12-07 21:39:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'Advanced System Design',
-    version = '0.1.0',
-    author = 'Zeevi Iosub',
-    description = 'Advanced System Design project - "man-machine inerface".',
-    packages = find_packages(),
-    tests_require = ['pytest', 'pytest-cov'],
+    name='Advanced System Design',
+    version='0.1.0',
+    author='Zeevi Iosub',
+    description='Advanced System Design project - "man-machine inerface".',
+    packages=find_packages(),
+    tests_require=['pytest', 'pytest-cov'],
 )
