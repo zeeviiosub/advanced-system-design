068d46fa89e7c07167f27740c96899968a3d9099
zeeviiosub <57300859+zeeviiosub@users.noreply.github.com>
2019-12-07 20:56:05 +0200

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,29 +1,29 @@
 class Connection:
     def __init__(self, socket):
         self.socket = socket
-    
+
     def __repr__(self):
         sockname = self.socket.getsockname()
         fromaddr = f'{sockname[0]}:{sockname[1]}'
         peername = self.socket.getpeername()
         toaddr = f'{peername[0]}:{peername[1]}'
         return f'<Connection from {fromaddr} to {toaddr}>'
-    
+
     def __enter__(self):
         return self
-    
+
     def __exit__(self, x, y, z):
         self.close()
-    
+
     def connect(host, port):
         import socket
         socket = socket.socket()
         socket.connect((host, port))
         return Connection(socket)
-    
+
     def send(self, data):
         self.socket.sendall(data)
-    
+
     def receive(self, size):
         data = self.socket.recv(size)
         while len(data) < size:
@@ -32,6 +32,6 @@ class Connection:
                 raise Exception('Incomplete message')
             data += more_data
         return data
-    
+
     def close(self):
         self.socket.close()
